d58a4de9612954b3c46721c752dd82023909ad53
Syed Mohammad Hassan <hassan.imroze@hotmail.com>
2019-01-07 12:10:23 +0600

    Small Business instalment payment

---
+++ CELSavings/ViewModels/SmallBusinessPaymentViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CELSavings.ViewModels
+{
+    public class SmallBusinessPaymentViewModel
+    {
+        public int SmallBusinessId { get; set; }
+        
+        public string Product { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public decimal SellingPrice { get; set; }
+
+        public decimal PaymentReceived { get; set; }
+        
+        public decimal PaymentDue { get; set; }
+
+        public decimal PaidAmount { get; set; }
+    }
+}
